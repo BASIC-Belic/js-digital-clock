b0e094e3704c6a37ead8d9627e39143b4b180931
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-16 15:54:59 -0800

    clock works

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+console.log('hi');
+$(document).ready( () => {
+
+  // const intervals = ["Hours", "Minutes", "Seconds"];
+
+  const clock = $('body #clock');
+  let date = new Date;
+
+  // intervals.forEach( interval => {
+  //   clock.append('<li>' + interval + ': ' + date.get`${interval}` + '</li>');
+  // })
+
+  const myClock = () => {
+
+    clock.append( '<p>' + `${date.getHours()}` + ':' + `${date.getMinutes()}` + ':' + `${date.getSeconds()}` + '</p>');
+  };
+
+  let intervalID = window.setInterval(myClock, 1000);
+
+});
