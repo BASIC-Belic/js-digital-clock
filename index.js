f8bcbf666149e5f5d461492b6a0e620b0d5637e0
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-19 09:14:46 -0800

    it works, thank god

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-// Your code here
-console.log('hi');
+
+
+
+
+
 $(document).ready( () => {
-
-  // const intervals = ["Hours", "Minutes", "Seconds"];
-
-  const clock = $('body #clock');
-  let date = new Date;
-
-  // intervals.forEach( interval => {
-  //   clock.append('<li>' + interval + ': ' + date.get`${interval}` + '</li>');
-  // })
 
   const myClock = () => {
 
-    clock.append( '<p>' + `${date.getHours()}` + ':' + `${date.getMinutes()}` + ':' + `${date.getSeconds()}` + '</p>');
+    // const intervals = ["Hours", "Minutes", "Seconds"];
+
+    // intervals.forEach( interval => {
+    //   clock.append('<li>' + interval + ': ' + date.get`${interval}` + '</li>');
+    // })
+
+    const clock = $('body #clock');
+    let date = new Date();
+    let hours = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+    let seconds = date.getSeconds().toString();
+    let timeofDay = "PM";
+
+    if (hours === 0) {
+      hours = 12;
+    }
+
+    if (hours < 12 ){
+      timeofDay = "AM";
+    }
+
+    if (hours > 12) {
+      hours -= 12;
+    }
+
+    if(hours < 10){
+      hours = "0" + hours ;
+    }
+
+    if(minutes < 10){
+      minutes = "0" + minutes;
+    }
+
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+
+    clock.html( hours + ' : ' + minutes + ' : ' + seconds + " " + timeofDay );
+
+    let hexColor = '#' + hours + minutes + seconds;
+
+    document.body.style.backgroundColor = hexColor;
+
   };
 
-  let intervalID = window.setInterval(myClock, 1000);
+  window.setInterval(myClock, 1000);
 
 });
